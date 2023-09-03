3aa913170654114d30d9ddb4ea2f5e886631b502
송양종 <ssongmantop@gmail.com>
2023-09-03 23:43:05 +0900

    update kafka src

--- kafka/SpringBootSample/kafka-sample-producer/src/main/java/com/ssongman/kafka/producer/controller/PubController.java
+++ kafka/SpringBootSample/kafka-sample-producer/src/main/java/com/ssongman/kafka/producer/controller/PubController.java
@@ -1,7 +1,7 @@
 package com.ssongman.kafka.producer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
